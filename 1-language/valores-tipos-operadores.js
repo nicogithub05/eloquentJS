9681bb738d90112dac67da4ef9552a5272fa3b97
nicogithub05 <nicolasgithub@gmail.com>
2024-08-15 14:45:50 -0500

    operadores unarios

--- 1-language/valores-tipos-operadores.js
+++ 1-language/valores-tipos-operadores.js
@@ -1,6 +1,5 @@
 // ------ Números -------
 
-
 let num = 13 // números
 // todos los valores son bits. en este caso la combinación de bits representan el número 13.
 let numFracc = 12.5 // números fraccionarios separados por puntos. 
@@ -33,4 +32,34 @@ console.log(concat);
 
 //podemos hacer uso de template literals para incluir variables o operaciones dentro de una cadena
 let templateLit = `la mitad de 100 es ${100 / 2}`
+
+// ----- Operadores  Unarios _----- 
+// actúan sobre un solo operando
+
+let una1 = typeof 5;
+console.log(una1); // Number
+
+let una2 = typeof "hola";
+console.log(una2) // string
+
+let incre = 6;
+console.log(incre++);
+
+let decre = 7; 
+console.log(decre--)
+
+let negLog = true; 
+let negFals = !negLog; //false 
+// invierte el valor booleano de su operando
+
+let positivo = 3;
+let negativo = -positivo; // negativo ahora es -3
+
+// en JS cualquier valor puede ser evaluado como verdadero o falso. 
+
+
+
+
+
+
 
