223dde2c663de55e08e225416ceb315379c915f5
nicogithub05 <nicolasgithub@gmail.com>
2024-08-15 14:09:49 -0500

    números

---
+++ 1-language/valores-tipos-operadores.js
@@ -0,0 +1,21 @@
+// ------ Números -------
+
+
+let num = 13 // números
+// todos los valores son bits. en este caso la combinación de bits representan el número 13.
+let numFracc = 12.5 // números fraccionarios separados por puntos. 
+// importante tratarlos como aproximaciones y no valores precisos. 
+
+let sum = 1 + 1;
+let rest = 1 - 1;
+let div = 2 / 1; 
+let mult = 2 * 2;
+
+let residuo = 8 % 2; // llamado residuo o módulo
+
+let infin = infinity;
+let negInf = -infinity; //no son tan exactos los cálculos
+
+let notANumber = NaN; //
+
+
